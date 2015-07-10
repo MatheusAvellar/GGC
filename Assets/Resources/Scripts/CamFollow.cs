e44a9e096c0d6b1088f802f22c601dfaeaf6bbb3
unknown <seeduc@NAVE035-EATEEC6.nave.local>
2015-07-10 09:16:17 -0300

    Updated camera movement

--- Assets/Resources/Scripts/CamFollow.cs
+++ Assets/Resources/Scripts/CamFollow.cs
@@ -5,13 +5,34 @@ public class CamFollow : MonoBehaviour {
 	
 	private GameObject player;
 	private Vector3 newPos;
+	private Vector3 newRot;
+	private bool canRot;
+	
+	void Start () {
+		newRot = new Vector3(30f, 45f, 0f);
+	}
 
 	void Update () {
 		player = GameObject.FindGameObjectWithTag ("Player") as GameObject;
 		newPos = new Vector3 (player.transform.position.x,
 		                      player.transform.position.y,
 		                      player.transform.position.z);
-		transform.position = Vector3.Lerp (this.transform.position, newPos, 5f * Time.deltaTime);
+		this.transform.position = Vector3.Lerp (this.transform.position, newPos, 5f * Time.deltaTime);
+		
+		
+		if (Input.GetKey(KeyCode.Q)) {
+			canRot = true;
+			newRot = new Vector3(30f, 135f, 0f);
+			
+		} else if (Input.GetKey(KeyCode.E)) {
+			canRot = false;
+			newRot = new Vector3(30f, 45f, 0f);
+		}
 
+		if (canRot) {
+			this.transform.localEulerAngles = Vector3.Lerp(this.transform.localEulerAngles, newRot, 5f * Time.deltaTime);
+		} else {
+			this.transform.localEulerAngles = Vector3.Lerp(this.transform.localEulerAngles, newRot, 5f * Time.deltaTime);
+		}
 	}
 }
